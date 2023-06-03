450e57e6f9e8f68994a9d0c7b391d9f22093af3b
Sacha Lansky <sacha@parity.io>
2023-06-03 19:54:09 +0200

    ⚡️ Update landing page copy

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,32 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Explore different use cases',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This learning hubs aims to be a place to explore different use cases of NFTs on Statemine, for end-users, businesses and prospective collection owners.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'About the author',
+    Svg: 'none',
+    description: (
+      <>
+        Hi my name is Sacha 👋 &mdash; I'm a Developer Advocate at Parity Technologies. I created this resource to 
+        help end-users and developers learn about the different NFT capabilities on Polkadot, with a particular 
+        focus on FRAME's new NFTs pallet.
+      </>
+    ),
+  },
+  {
+    title: 'Explore ways to use the NFTs pallet in your app-chain',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        This learning hub also aims to be a place containing examples for runtime engineers 
+        curious to use the pallet in their parachains.
       </>
     ),
   },
